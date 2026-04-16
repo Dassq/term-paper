0d0a8f2cad925a94a5809eac0f03882677601408
Dasssha09 <157128967+Dassq@users.noreply.github.com>
2026-04-16 18:19:48 +0700

    Create config.php

---
+++ my-db/config.php
@@ -0,0 +1,10 @@
+<?php
+if (count(get_included_files()) === 1) exit("Доступ запрещен");
+
+return [
+    'host'    => 'localhost',
+    'db'      => 'dasspag8_1',
+    'user'    => 'dasspag8_1',
+    'pass'    => 'dasspag8_',
+    'charset' => 'utf8mb4'
+];
